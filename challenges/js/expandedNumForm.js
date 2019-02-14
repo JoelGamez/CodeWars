b507ac4f8df65ac3f3cd60014c72faee3f325a85
JoelGamez <37604070+JoelGamez@users.noreply.github.com>
2019-02-14 08:01:44 -0500

    Update expandedNumForm.js

--- challenges/js/expandedNumForm.js
+++ challenges/js/expandedNumForm.js
@@ -1,5 +1,11 @@
 // https://www.codewars.com/kata/write-number-in-expanded-form/train/javascript
 
+/*You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'*/
+
 function expandedForm(num) {
   let answer = "";
   let zero = "0";
